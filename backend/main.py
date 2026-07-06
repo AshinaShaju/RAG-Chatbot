4b4b84218da4319ee80ce4bcf91c6a5f25676fe1
Ashina Shaju <ashinashaju12@gmail.com>
2026-07-06 20:07:06 +0530

    Built first FastAPI chat endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class ChatRequest(BaseModel):
+    message: str
+
+
 @app.get("/")
 def home():
-    return {"message": "Hello Ashina!"}
+    return {"message": "Welcome to my RAG Chatbot!"}
+
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+    return {
+        "response": f"You said: {request.message}"
+    }
